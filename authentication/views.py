d3f441bd5baa4d777922c0be9ca6ab8843d3de0b
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-10-02 09:57:56 +0300

    Introduce logging functionality to record important project events and errors for improved project maintenance

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,13 @@
+import logging
+
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import RegistrationSerializer, LoginSerializer
+
+
+logger = logging.getLogger(__name__)
 
 
 class RegistrationView(generics.CreateAPIView):
@@ -13,7 +18,14 @@ class RegistrationView(generics.CreateAPIView):
 
         if serializer.is_valid():
             serializer.save()
+
+            # Log a successful registration
+            logger.info(f"User registered successfully: {serializer.validated_data['username']}")
+
             return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+
+        # Log a registration failure
+        logger.error(f"Registration failed: {serializer.errors}")
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -25,6 +37,13 @@ class LoginView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
+            # Log a successful login
+            logger.info(f"User logged in successfully: {serializer.validated_data['email']}")
+
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+        # Log a login failure
+        logger.error("Login failed")
+
         return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
