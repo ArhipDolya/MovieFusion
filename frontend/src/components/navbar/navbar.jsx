64cb01a6dad6287328222a14257da088d7e66859
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-10-03 10:17:12 +0300

    Add navigation bar component

---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import './navbar.css'
+
+
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    return (
+        
+        <nav>
+            <Link to="/" className="title">
+                MovieFusion
+            </Link>
+            <div className='menu' onClick={() => {
+                setMenuOpen(!menuOpen)
+            }}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ""}>
+                <li>
+                    <NavLink to="/movies">Movies</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/categories">Categories</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/Sign-in">Sign In</NavLink>
+                </li>
+            </ul>
+        </nav>
+        
+    );
+}
+
+export default Navbar;
