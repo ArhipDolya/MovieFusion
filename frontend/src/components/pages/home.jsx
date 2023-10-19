1366c676951dbd0db85c36173b2561b28874ee34
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-10-19 10:32:50 +0300

    Add some styling for movies.css file and create show more button functionality for the home page to show every 9 movies on the screen

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [moviesToShow, setMoviesToShow] = useState(9);
+  const [visibleMovies, setVisibleMovies] = useState([]);
 
   useEffect(() => {
     // Fetch Movies
@@ -21,6 +23,16 @@ export const Home = () => {
       });
   }, []);
 
+  useEffect(() => {
+    // Update the visible movies when moviesToShow changes
+    setVisibleMovies(movies.slice(0, moviesToShow));
+  }, [moviesToShow, movies])
+
+  const loadMoreMovies = () => {
+    // Increase the number of movies to show
+    setMoviesToShow((prevCount) => prevCount + 9)
+  }
+
   return (
     <div className="home-container">
       <h1 className="movie-world">Welcome to Movie World</h1>
@@ -28,7 +40,7 @@ export const Home = () => {
       <section>
         <div className="movie-list">
           <div className="scrolling-container">
-            {movies.map((movie) => (
+            {visibleMovies.map((movie) => (
               <div key={movie.id} className="movie-card">
                 <Link to={`/movie/${movie.slug}/`}>
                   <img src={movie.image} alt={movie.title} />
@@ -39,6 +51,12 @@ export const Home = () => {
           </div>
         </div>
       </section>
+
+      {moviesToShow < movies.length && (
+        <button className="show-more-button" onClick={loadMoreMovies}>
+          Show More
+        </button>
+      )}
     </div>
   );
 };
