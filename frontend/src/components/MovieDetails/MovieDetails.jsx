2ce69dfc031196519acb5d4e0be2d2d7dee39a94
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-11-03 14:45:35 +0200

    Change styling for delete comment button

--- frontend/src/components/MovieDetails/MovieDetails.jsx
+++ frontend/src/components/MovieDetails/MovieDetails.jsx
@@ -261,9 +261,8 @@ const MovieDetails = () => {
                     </div>
                     <p className="text-gray-600 mt-2">{comment.text}</p>
                     
-                    <button
-                      onClick={() => handleDeleteComment(comment.id)}
-                      className="text-red-500 hover:text-red-700 cursor-pointer mt-2">
+                     <button onClick={() => handleDeleteComment(comment.id)}
+                      className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                       Delete
                     </button>
 
